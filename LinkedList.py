38409e2d9c77ccaee3cb68b904ff1fbef2016e4a
Alireza <a.helali1995@example.com>
2020-12-16 13:45:28 +0330

    implementation of LinkedList

---
+++ LinkedList.py
@@ -0,0 +1,98 @@
+class LinkedList:
+    class Node:
+        def __init__(self, value):
+            self.value = value
+            self.next = None
+
+    def __init__(self):
+        self.start = None
+        self.last = None
+
+    def isnone(self):
+        if self.start is None:
+            return True
+
+    def add_first(self, value):
+        node = LinkedList.Node(value)
+        if self.isnone():
+            self.start = node
+        else:
+            node.next = self.start
+            self.start = node
+
+    def add_last(self, value):
+        node = LinkedList.Node(value)
+        if self.isnone():
+            self.start = node
+            self.last = node
+        else:
+            self.last.next = node
+            self.last = node
+
+    def index_of(self, value):
+        node = LinkedList.Node(value)
+        temp = self.start
+        index = 0
+        while temp != self.last:
+            if temp.value == node.value:
+                return index
+            temp = temp.next
+            index += 1
+        return -1
+
+    def contains(self, value):
+        if self.index_of(value) != -1:
+            return True
+        else:
+            return False
+
+    def remove_first(self):
+        if self.isnone():
+            raise ValueError('LinkedList is Empty')
+        elif self.start == self.last:
+            self.start = self.last = None
+        else:
+            self.start = self.start.next
+
+    def remove_last(self):
+        if self.isnone():
+            raise ValueError('LinkedList is Empty')
+        elif self.start == self.last:
+            self.start = self.last = None
+        else:
+            temp = self.start
+            while temp.next != self.last:
+                temp = temp.next
+            temp.next = None
+            self.last = temp
+
+    def length(self):
+        if self.isnone():
+            raise ValueError('LinkedList is Empty')
+        size = 1
+        temp = self.start
+        while temp != self.last:
+            temp = temp.next
+            size += 1
+        return size
+
+    def __str__(self):
+        if self.isnone():
+            raise ValueError('LinkedList is Empty')
+        temp = self.start
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        return 'done'
+
+
+l = LinkedList()
+l.add_last(10)
+l.add_last(20)
+l.add_last(30)
+l.add_last(40)
+l.add_first(50)
+l.remove_first()
+l.remove_last()
+print(l.length())
+print(l)
